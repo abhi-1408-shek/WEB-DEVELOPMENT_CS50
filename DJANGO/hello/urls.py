7c37f681b7f5809049b83dea6fc273feb42b5a15
Yawar Azad <yawarazad@gmail.com>
2024-10-29 14:20:24 +0530

    bugfix: change urlpatterns from set to list

--- DJANGO/hello/urls.py
+++ DJANGO/hello/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-urlpatterns = {
+urlpatterns = [
     path("", views.index, name="index"),
     path("brian", views.brian, name = "brian"),
     path("david", views.david, name="david"),
     path("<str:name>", views.greet, name="greet")
-}
+]
