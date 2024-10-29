db7aafaf230936f80d7bdcfaa0b2ea8e79a14006
Abhishek <abhishekpooja1408@gmail.com>
2024-10-29 10:39:37 +0530

    adding more client side validation forms

--- DJANGO/tasks/views.py
+++ DJANGO/tasks/views.py
@@ -1,7 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="NewTask")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
@@ -10,4 +14,6 @@ def index(request):
     
 # Add a new task:
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html",{
+        "form" : NewTaskForm()
+    })
